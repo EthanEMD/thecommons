619266277f1208e69e2cfacd7f551bfbeba4ef69
EthanEMD <139723384+EthanEMD@users.noreply.github.com>
2023-09-20 17:39:58 +1000

    Update location-api-script.js

--- location-api-script.js
+++ location-api-script.js
@@ -1,25 +1,25 @@
 
     
-    // // Your geoip function
-    // function geoip(json) {
-    //     // Log the received JSON data to the console
-    //     console.log('Received JSON data:', json);
-    //     // Extract the region property from the JSON object
-    //     var region = json.region;
-    //     // Log the region to the console
-    //     console.log('Region:', region);
-    //     // Determine the state based on the region, default to VIC if not recognized
-    //     var state = "vic"; // Default to VIC
-    //     if (region === "New South Wales" || region === "NSW") {
-    //         state = "nsw";
-    //     } else if (region === "Victoria" || region === "VIC") {
-    //         state = "vic";
-    //     } else {
-    //         // Log a message if the region is not recognized
-    //         console.log('Unrecognized region:', region);
-    //     }
-    //     // Log the determined state to the console
-    //     console.log('Determined State:', state);
+    // Your geoip function
+    function geoip(json) {
+        // Log the received JSON data to the console
+        console.log('Received JSON data:', json);
+        // Extract the region property from the JSON object
+        var region = json.region;
+        // Log the region to the console
+        console.log('Region:', region);
+        // Determine the state based on the region, default to VIC if not recognized
+        var state = "vic"; // Default to VIC
+        if (region === "New South Wales" || region === "NSW") {
+            state = "nsw";
+        } else if (region === "Victoria" || region === "VIC") {
+            state = "vic";
+        } else {
+            // Log a message if the region is not recognized
+            console.log('Unrecognized region:', region);
+        }
+        // Log the determined state to the console
+        console.log('Determined State:', state);
 
     //     // Function to select the div element based on the determined state
     //     function clickDivElement(state) {
