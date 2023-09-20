b71012d95f31781d79a86d7c83773557ebbc3147
EthanEMD <139723384+EthanEMD@users.noreply.github.com>
2023-09-20 17:44:48 +1000

    Update location-api-script.js

--- location-api-script.js
+++ location-api-script.js
@@ -1,4 +1,43 @@
+<script>
+// Wrap your code inside a DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('DOMContentLoaded event triggered.'); // Log a message when the DOMContentLoaded event is triggered
+// Your geoip function
+    function geoip(json) {
+        // Log the received JSON data to the console
+        console.log('Received JSON data:', json);
+        // Extract the region property from the JSON object
+        var region = json.region;
+        // Log the region to the console
+        console.log('Region:', region);
+        // Determine the state based on the region, default to VIC if not recognized
+        var state = "vic"; // Default to VIC
+        if (region === "New South Wales" || region === "NSW") {
+            state = "nsw";
+        } else if (region === "Victoria" || region === "VIC") {
+            state = "vic";
+        } else {
+            // Log a message if the region is not recognized
+            console.log('Unrecognized region:', region);
+        }
+        // Log the determined state to the console
+        console.log('Determined State:', state);
 
+        // Function to select the div element based on the determined state
+        function clickDivElement(state) {
+            const divElement = document.getElementById(state + '-radio');
+            if (divElement) {
+                divElement.click();
+            } else {
+                // Log a message if the div element is not found
+                console.log('Div element not found for state:', state);
+            }
+        }
+
+        // Click the div element based on the determined state
+        clickDivElement(state);
+    }
+</script>
     
     // Your geoip function
     function geoip(json) {
@@ -35,6 +74,7 @@
         // Click the div element based on the determined state
         clickDivElement(state);
     }
+    
 
     // // Function to handle intersection changes for the map-section div
     // function handleMapSectionIntersection(entries) {
@@ -64,4 +104,6 @@
     //     // Log a message if the map-section div is not found
     //     console.log('Map section not found.');
     // }
+
+});
 
