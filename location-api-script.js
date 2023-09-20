74e21e180da5f3f9d2be86668a1bc46924330bf8
EthanEMD <139723384+EthanEMD@users.noreply.github.com>
2023-09-21 08:29:22 +1000

    Update location-api-script.js

--- location-api-script.js
+++ location-api-script.js
@@ -1,44 +1,7 @@
-<script>
 // Wrap your code inside a DOMContentLoaded event listener
 document.addEventListener("DOMContentLoaded", function () {
     console.log('DOMContentLoaded event triggered.'); // Log a message when the DOMContentLoaded event is triggered
-// Your geoip function
-    function geoip(json) {
-        // Log the received JSON data to the console
-        console.log('Received JSON data:', json);
-        // Extract the region property from the JSON object
-        var region = json.region;
-        // Log the region to the console
-        console.log('Region:', region);
-        // Determine the state based on the region, default to VIC if not recognized
-        var state = "vic"; // Default to VIC
-        if (region === "New South Wales" || region === "NSW") {
-            state = "nsw";
-        } else if (region === "Victoria" || region === "VIC") {
-            state = "vic";
-        } else {
-            // Log a message if the region is not recognized
-            console.log('Unrecognized region:', region);
-        }
-        // Log the determined state to the console
-        console.log('Determined State:', state);
 
-        // Function to select the div element based on the determined state
-        function clickDivElement(state) {
-            const divElement = document.getElementById(state + '-radio');
-            if (divElement) {
-                divElement.click();
-            } else {
-                // Log a message if the div element is not found
-                console.log('Div element not found for state:', state);
-            }
-        }
-
-        // Click the div element based on the determined state
-        clickDivElement(state);
-    }
-</script>
-    
     // Your geoip function
     function geoip(json) {
         // Log the received JSON data to the console
@@ -48,11 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
         // Log the region to the console
         console.log('Region:', region);
         // Determine the state based on the region, default to VIC if not recognized
-        var state = "vic"; // Default to VIC
+        var state = "VIC"; // Default to VIC
         if (region === "New South Wales" || region === "NSW") {
-            state = "nsw";
+            state = "NSW";
         } else if (region === "Victoria" || region === "VIC") {
-            state = "vic";
+            state = "VIC";
         } else {
             // Log a message if the region is not recognized
             console.log('Unrecognized region:', region);
@@ -60,50 +23,17 @@ document.addEventListener("DOMContentLoaded", function () {
         // Log the determined state to the console
         console.log('Determined State:', state);
 
-        // Function to select the div element based on the determined state
-        function clickDivElement(state) {
-            const divElement = document.getElementById(state + '-radio');
-            if (divElement) {
-                divElement.click();
-            } else {
-                // Log a message if the div element is not found
-                console.log('Div element not found for state:', state);
-            }
-        }
+        // Update the query parameter based on the determined state
+        var queryParams = new URLSearchParams(window.location.search);
+        queryParams.set('state', state);
 
-        // Click the div element based on the determined state
-        clickDivElement(state);
+        // Update the URL with the new query parameter
+        var newURL = window.location.pathname + '?' + queryParams.toString();
+        history.replaceState(null, '', newURL);
+
+        // You can optionally trigger any other actions or update the UI as needed here
     }
-    
 
-    // // Function to handle intersection changes for the map-section div
-    // function handleMapSectionIntersection(entries) {
-    //     entries.forEach((entry) => {
-    //         if (entry.isIntersecting) {
-    //             // Call the geoip function when the map-section div is in view
-    //             geoip(/* your JSON data */);
-    //             // Disconnect the observer to prevent further triggers
-    //             observer.disconnect();
-    //         }
-    //     });
-    // }
-
-    // // Create an Intersection Observer to watch the map-section div
-    // const observer = new IntersectionObserver(handleMapSectionIntersection, {
-    //     root: null, // Use the viewport as the root
-    //     threshold: 0.5, // Trigger when at least 50% of the element is in view
-    // });
-
-    // // Select the map-section div by its ID
-    // const mapSection = document.getElementById("map-section");
-
-    // if (mapSection) {
-    //     // Start observing the map-section div
-    //     observer.observe(mapSection);
-    // } else {
-    //     // Log a message if the map-section div is not found
-    //     console.log('Map section not found.');
-    // }
-
+    // Call the geoip function when the DOM is ready
+    geoip({ region: 'Victoria' }); // Replace with your actual GeoIP data
 });
-
