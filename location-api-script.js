9a79ff836fc280b98af0cbc19e9410ee1461922e
EthanEMD <139723384+EthanEMD@users.noreply.github.com>
2023-09-20 16:10:28 +1000

    Update location-api-script.js

--- location-api-script.js
+++ location-api-script.js
@@ -1,4 +1,3 @@
-
 function geoip(json) {
     // Log the received JSON data to the console
     console.log('Received JSON data:', json);
@@ -18,23 +17,25 @@ function geoip(json) {
     }
     // Log the determined state to the console
     console.log('Determined State:', state);
+
     // Function to select the div element based on the determined state
     function clickDivElement(state) {
         const divElement = document.getElementById(state + '-radio');
         if (divElement) {
             divElement.click();
+            
+            // Assuming the map section has an ID like "map-section"
+            // Set its visibility to "visible" after the click
+            const mapSection = document.getElementById("map-section");
+            if (mapSection) {
+                mapSection.style.visibility = "visible";
+            }
         } else {
             // Log a message if the div element is not found
             console.log('Div element not found for state:', state);
         }
     }
+
     // Click the div element based on the determined state
     clickDivElement(state);
-}
-
-window.onload = function() {
-    setTimeout(function() {
-        // Scroll to the top of the page
-        window.scrollTo(0, 0);
-    }, 0);
 }
