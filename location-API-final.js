8bd158c565e8477cbd6115fc710301605156b23a
EthanEMD <139723384+EthanEMD@users.noreply.github.com>
2023-09-21 10:17:10 +1000

    Update location-API-final.js

--- location-API-final.js
+++ location-API-final.js
@@ -1,28 +1,4 @@
-function geoip(json) {
-    // Log the received JSON data to the console
-    console.log('Received JSON data:', json);
-    
-    // Extract the region property from the JSON object
-    var region = json.region;
-    
-    // Log the region to the console
-    console.log('Region:', region);
-    
-    // Determine the state based on the region, default to "VIC" if not recognized
-    var state = "VIC"; // Default to VIC
-    
-    if (region === "New South Wales" || region === "NSW") {
-        state = "NSW";
-    } else if (region === "Victoria" || region === "VIC") {
-        state = "VIC";
-    } else {
-        // Log a message if the region is not recognized
-        console.log('Unrecognized region:', region);
-    }
-    
-    // Log the determined state to the console
-    console.log('Determined State:', state);
-
+document.addEventListener("DOMContentLoaded", function () {
     // Function to click on the div element based on the determined state
     function clickDivElement(state) {
         // Select the div element with a custom attribute "data-state"
@@ -47,6 +23,44 @@ function geoip(json) {
         }
     }
 
-    // Click the div element based on the determined state
-    clickDivElement(state);
-}
+    // Load the JSON data using the external script
+    // This will invoke handleGeoJsJson with the JSON data when it's ready
+    var geoJsScript = document.createElement("script");
+    geoJsScript.src = "https://get.geojs.io/v1/ip/geo.js";
+    document.head.appendChild(geoJsScript);
+
+    // Define a callback function to handle the JSON data
+    function handleGeoJsJson(json) {
+        // Log the received JSON data to the console
+        console.log('Received JSON data:', json);
+
+        // Extract the region property from the JSON object
+        var region = json.region;
+
+        // Log the region to the console
+        console.log('Region:', region);
+
+        // Determine the state based on the region, default to "VIC" if not recognized
+        var state = "VIC"; // Default to VIC
+
+        // Map region names to states
+        const regionToState = {
+            "New South Wales": "NSW",
+            "Victoria": "VIC",
+        };
+
+        // Check if the region is recognized in the mapping
+        if (regionToState.hasOwnProperty(region)) {
+            state = regionToState[region];
+        } else {
+            // Log a message if the region is not recognized
+            console.log('Unrecognized region:', region);
+        }
+
+        // Log the determined state to the console
+        console.log('Determined State:', state);
+
+        // Click the div element based on the determined state
+        clickDivElement(state);
+    }
+});
